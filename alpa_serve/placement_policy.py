991c1227e2b10da5172c82285c66a17da6ab0f5f
Lianmin Zheng <lianminzheng@gmail.com>
2022-10-13 02:25:45 +0000

    add placement policy

--- alpa_serve/placement_policy.py
+++ alpa_serve/placement_policy.py
@@ -1,8 +1,9 @@
+"""Model placement policy"""
 from collections import namedtuple
 from dataclasses import dataclass
 import multiprocessing
 import time
-from typing import List
+from typing import List, Tuple
 
 import numpy as np
 import pulp
@@ -15,6 +16,7 @@ class ModelData:
     model_mem: float
     average_load: float
     single_throughput: float
+    pipeline_decay: List[Tuple[int, float]] = None
 
 
 ParallelConfig = namedtuple("ParallelConfig", ("dp", "mp", "pp"))
@@ -27,6 +29,7 @@ class PlacementPolicy:
 class SelectiveReplication(PlacementPolicy):
     def __init__(self, verbose: bool = False):
         self.verbose = verbose
+        self.time_limit = 20
 
     def place_models(self,
                      controller,
@@ -38,57 +41,61 @@ class SelectiveReplication(PlacementPolicy):
         for g_id in range(num_gpus):
             controller.launch_mesh_group_manager.remote(g_id, [1, 1])
 
+        tasks = []
         for m_id in range(len(model_datas)):
             for g_id in range(num_gpus):
                 if placement[m_id][g_id]:
                     name = model_datas[m_id].name
-                    controller.create_replica.remote(
-                        name, g_id, (ParallelConfig(1, 1, 1),))
+                    tasks.append(controller.create_replica.remote(
+                        name, g_id, (ParallelConfig(1, 1, 1),)))
+        return tasks
 
     def solve(self,
-              mem_budget: float,
+              model_datas: List[ModelData],
               num_gpus: int,
-              model_datas: List[ModelData]):
+              mem_budget: float):
         tic = time.time()
 
-        num_models = len(model_datas)
+        # Load constants
+        N = len(model_datas)
+        M = num_gpus
+        C = mem_budget
         a = [x.average_load for x in model_datas]
-        m = [x.model_mem for x in model_datas]
-        s = [x.single_throughput for x in model_datas]
+        c = [x.model_mem for x in model_datas]
+        t = [x.single_throughput for x in model_datas]
 
         # 1. Create variables
-        p = LpVariable.matrix(
-            "p", (range(num_models), range(num_gpus)), cat="Binary")
-        rep = LpVariable.matrix(
-            "rep", (range(num_models),), cat="Integer")
-        min_tolerance = LpVariable("min_tolerance")
+        p = LpVariable.matrix("p", (range(N), range(M)), cat="Binary")
+        rep = [None] * N
+        min_tolerance = LpVariable("min_tolerance", lowBound=0)
+        sum_tolerance = LpVariable("sum_tolerance", lowBound=0)
 
         # 2. Objective
         prob = LpProblem("myProblem", LpMaximize)
-        obj = min_tolerance
+        obj = min_tolerance  #+ 0.001 * sum_tolerance
         prob += obj
 
         # 3. Constraints
         # (a). memory budget on each GPU
-        for j in range(num_gpus):
-            prob += lpSum(p[i][j] * m[i] for i in range(num_models)) <= mem_budget
+        for j in range(M):
+            prob += lpSum(p[i][j] * c[i] for i in range(N)) <= C
 
         # (b). number of replicas
-        for i in range(num_models):
-            prob += rep[i] == lpSum(p[i][j] for j in range(num_gpus))
+        for i in range(N):
+            rep[i] = lpSum(p[i][j] for j in range(M))
 
-        # (c). min tolerance
-        for i in range(num_models):
-            prob += min_tolerance <= rep[i] * s[i] / a[i]
+        # (c). min tolerance and sum tolerance
+        for i in range(N):
+            prob += min_tolerance <= rep[i] * (t[i] / a[i])
+        #prob += sum_tolerance == lpSum(rep[i] * (t[i] / a[i]) for i in range(N))
 
         msg = self.verbose
-        time_limit = 600
         assert "PULP_CBC_CMD" in pulp.listSolvers(onlyAvailable=True), (
             "Please install ILP solvers by 'sudo apt install coinor-cbc'")
 
         solver = pulp.PULP_CBC_CMD(mip=True,
                                    msg=msg,
-                                   timeLimit=time_limit,
+                                   timeLimit=self.time_limit,
                                    threads=multiprocessing.cpu_count())
         prob.solve(solver)
 
@@ -104,10 +111,140 @@ class SelectiveReplication(PlacementPolicy):
                 "Cannot run the function under the given memory budget. "
                 "Please increase the memory budget.")
 
-        p_res = np.zeros((num_models, num_gpus), dtype=np.int8)
-        for i in range(num_models):
-            for j in range(num_gpus):
+        p_res = np.zeros((N, M), dtype=np.int8)
+        for i in range(N):
+            for j in range(M):
                 if pulp.value(p[i][j]):
                     p_res[i][j] = 1
 
-        return p_res
+        return objective, p_res
+
+
+class SelectiveReplicationWithPipeline(PlacementPolicy):
+    def __init__(self, verbose: bool = False):
+        self.verbose = verbose
+        self.time_limit = 20
+
+    def solve(self,
+              model_datas: List[ModelData],
+              num_gpus: int,
+              mem_budget: float,
+              group_sizes: List[int]):
+        tic = time.time()
+
+        # Load constants
+        N = len(model_datas)
+        M = num_gpus
+        C = mem_budget
+        a = [x.average_load for x in model_datas]
+        c = [x.model_mem for x in model_datas]
+        t = [x.single_throughput for x in model_datas]
+
+        G = num_gpus
+        K = len(group_sizes)
+        g = group_sizes
+        d = np.zeros((N, K))
+        for i in range(N):
+            for group_size, decay in model_datas[i].pipeline_decay:
+                try:
+                    k = g.index(group_size)
+                except ValueError:
+                    k = -1
+
+                if k >= 0:
+                    d[i][k] = decay
+
+        # 1. Create variables
+        p = LpVariable.matrix("p", (range(N), range(G)), cat="Binary")
+        rep = [None] * N
+        min_tolerance = LpVariable("min_tolerance", lowBound=0)
+        sum_tolerance = LpVariable("sum_tolerance", lowBound=0)
+        s = LpVariable.matrix("s", (range(G), range(K)), cat="Binary")
+        pxs = LpVariable.matrix("pxs", (range(N), range(G), range(K)), cat="Binary")
+
+        # 2. Objective
+        prob = LpProblem("myProblem", LpMaximize)
+        obj = min_tolerance  #+ 0.001 * sum_tolerance
+        prob += obj
+
+        # 3. Constraints
+        # (a). memory budget on each GPU
+        for j in range(G):
+            prob += (lpSum(p[i][j] * c[i] for i in range(N)) <=
+                     C * lpSum(s[j][k] * g[k] for k in range(K)))
+
+        # (b). number of replicas
+        for i in range(N):
+            rep[i] = lpSum(pxs[i][j][k] * g[k] * d[i][k]
+                           for j in range(G) for k in range(K))
+
+        # (c). min tolerance and sum tolerance
+        for i in range(N):
+            prob += min_tolerance <= rep[i] * t[i] / a[i]
+
+        #prob += sum_tolerance == lpSum(rep[i] * t[i] / a[i] for i in range(N))
+
+        # (d). group size
+        prob += lpSum(s[j][k] * g[k] for j in range(G) for k in range(K)) == M
+
+        # (e). only one configuration
+        for j in range(G):
+            prob += lpSum(s[j][k] for k in range(K)) == 1
+
+        # (f). linearization
+        for i in range(N):
+            for j in range(G):
+                for k in range(K):
+                    prob += pxs[i][j][k] <= p[i][j]
+                    prob += pxs[i][j][k] <= s[j][k]
+                    prob += pxs[i][j][k] >= p[i][j] + s[j][k] - 1
+
+        msg = self.verbose
+        assert "PULP_CBC_CMD" in pulp.listSolvers(onlyAvailable=True), (
+            "Please install ILP solvers by 'sudo apt install coinor-cbc'")
+
+        solver = pulp.PULP_CBC_CMD(mip=True,
+                                   msg=msg,
+                                   timeLimit=self.time_limit,
+                                   threads=multiprocessing.cpu_count())
+        #solver = pulp.GLPK_CMD(mip=True, msg=msg)
+        prob.solve(solver)
+
+        status = prob.status
+        objective = pulp.value(prob.objective)
+        objective = float(objective) if objective is not None else -1.0
+        if self.verbose:
+            print(f"ILP Status: {LpStatus[status]}\tObjective: {objective}\t"
+                  f"Time: {time.time() - tic}")
+
+        if prob.status in [pulp.LpStatusInfeasible]:
+            raise RuntimeError(
+                "Cannot run the function under the given memory budget. "
+                "Please increase the memory budget.")
+
+        s_res = []
+        for j in range(G):
+            assert sum(pulp.value(s[j][k]) for k in range(K)) == 1
+            for k in range(K):
+                if pulp.value(s[j][k]):
+                    s_res.append(k)
+
+        p_res = np.zeros((N, G), dtype=np.int8)
+        for i in range(N):
+            for j in range(G):
+                if pulp.value(p[i][j]):
+                    p_res[i][j] = 1
+
+        group_sizes = []
+        group_models = []
+        for j in range(G):
+            group_size = g[s_res[j]]
+            if group_size:
+                tmp = []
+                for i in range(N):
+                    if p_res[i][j]:
+                        tmp.append(i)
+                group_sizes.append(group_size)
+                group_models.append(tmp)
+
+        return objective, (group_sizes, group_models)
--- tests/serve/test_placement_policy.py
+++ tests/serve/test_placement_policy.py
@@ -3,7 +3,9 @@ import unittest
 
 import numpy as np
 
-from alpa_serve.placement_policy import (SelectiveReplication, ModelData)
+from alpa_serve.placement_policy import (SelectiveReplication, ModelData,
+    SelectiveReplicationWithPipeline)
+from alpa.util import GB
 
 
 class PlacementPolicyTest(unittest.TestCase):
@@ -11,26 +13,44 @@ class PlacementPolicyTest(unittest.TestCase):
     def test_selective_replication(self):
         policy = SelectiveReplication()
 
-        mem_budget = 2
+        mem_budget = 2 * GB
         num_gpus = 4
         model_datas = [
-            ModelData("m1", 1.0, 1.0, 1.0),
-            ModelData("m2", 1.0, 1.0, 1.0),
-            ModelData("m3", 1.0, 1.0, 1.0),
-            ModelData("m4", 1.0, 1.0, 1.0),
+            ModelData("m1", 1.0 * GB, 1.0, 1.0),
+            ModelData("m2", 1.0 * GB, 1.0, 1.0),
+            ModelData("m3", 1.0 * GB, 1.0, 1.0),
+            ModelData("m4", 1.0 * GB, 1.0, 1.0),
         ]
 
-        placement = policy.solve(mem_budget, num_gpus, model_datas)
+        obj, placement = policy.solve(model_datas, num_gpus, mem_budget)
 
         row_sum = np.sum(placement, axis=1)
         col_sum = np.sum(placement, axis=0)
         assert row_sum.tolist() == [2, 2, 2, 2]
         assert col_sum.tolist() == [2, 2, 2, 2]
 
+    def test_selective_replication_with_pipeline(self):
+        mem_budget = 2 * GB
+        num_gpus = 4
+        model_datas = [
+            ModelData("m1", 1.0 * GB, 1.0, 1.0, [(1, 1), (2, 0.95), (4, 0.9)]),
+            ModelData("m2", 1.0 * GB, 1.0, 1.0, [(1, 1), (2, 0.95), (4, 0.9)]),
+            ModelData("m3", 1.0 * GB, 1.0, 1.0, [(1, 1), (2, 0.95), (4, 0.9)]),
+            ModelData("m4", 1.0 * GB, 1.0, 1.0, [(1, 1), (2, 0.95), (4, 0.9)]),
+        ]
+
+        policy = SelectiveReplicationWithPipeline()
+        obj, (group_sizes, group_models) = policy.solve(
+            model_datas, num_gpus, mem_budget, [0, 1, 2, 4])
+
+        assert group_sizes == [2, 2]
+        assert group_models == [[0, 1, 2, 3], [0, 1, 2, 3]]
+
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(PlacementPolicyTest("test_selective_replication"))
+    suite.addTest(PlacementPolicyTest("test_selective_replication_with_pipeline"))
     return suite
 
 
