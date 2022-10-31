44f84488b809694a68f507de475c901be5bc7959
Lianmin Zheng <lianminzheng@gmail.com>
2022-10-31 11:16:08 +0000

    tmp checkpoint

--- alpa_serve/placement_policy.py
+++ alpa_serve/placement_policy.py
@@ -8,14 +8,21 @@ import numpy as np
 import pulp
 from pulp import LpVariable, LpProblem, LpMaximize, lpSum, LpStatus
 
+from alpa_serve.profiling import ParallelConfig, ProfilingResult
+
 
 @dataclasses.dataclass
 class ModelData:
     name: str
-    model_mem: float
+    slo: float
     average_load: float
-    single_throughput: float
-    pipeline_decay: List[Tuple[int, float]] = None
+    profiling_result: ProfilingResult
+
+
+@dataclasses.dataclass
+class ClusterEnv:
+    num_gpus: int
+    mem_budget: float
 
 
 class PlacementPolicy:
@@ -25,43 +32,57 @@ class PlacementPolicy:
 class SelectiveReplication(PlacementPolicy):
     def __init__(self, verbose: bool = False):
         self.verbose = verbose
+
+        self.max_bs = 1
         self.time_limit = 20
         self.sum_k = 1e-4
 
     def place_models(self,
                      controller,
                      model_datas: List[ModelData],
-                     num_gpus: int,
-                     mem_budget: float):
-        obj, placement = self.solve(model_datas, num_gpus, mem_budget)
+                     cluster_env: ClusterEnv):
+        obj, placement = self.solve(model_datas, cluster_envs)
 
         # Launch mesh groups
         for g_id in range(num_gpus):
             controller.launch_mesh_group_manager.remote(g_id, [1, 1])
 
         # Launch model replicas
-        tasks = []
         for m_id in range(len(model_datas)):
             for g_id in range(num_gpus):
                 if placement[m_id][g_id]:
                     name = model_datas[m_id].name
-                    tasks.append(controller.create_replica.remote(
-                        name, g_id, [ParallelConfig(1, 1, 1)]))
-        return tasks
+                    controller.create_replica.remote(
+                        name, g_id, [ParallelConfig(1, 1, 1)])
+
+        controller.sync()
+
+    def compute_sinlge_throuhput(self, model_data):
+        parallel_config = ParallelConfig(1, 1, 1)
+        stage_latency = model_data.profiling_result.para_dict[
+            parallel_config].latency
+
+        single_throughput = 0
+        for b, (s,) in stage_latency.items():
+            if b > self.max_bs:
+                continue
+
+            single_throughput = max(single_throughput, 1 / s)
+        return single_throughput
 
     def solve(self,
               model_datas: List[ModelData],
-              num_gpus: int,
-              mem_budget: float):
+              cluster_env: ClusterEnv):
         tic = time.time()
 
         # Load constants
         N = len(model_datas)
-        M = num_gpus
-        C = 1
+        M = cluster_env.num_gpus
+        C = cluster_env.mem_budget
         a = [x.average_load for x in model_datas]
-        c = [x.model_mem / mem_budget for x in model_datas]
-        t = [x.single_throughput for x in model_datas]
+        c = [x.profiling_result.para_dict[ParallelConfig(1,1,1)].weight_mem[0]
+             for x in model_datas]
+        t = [self.compute_sinlge_throuhput(x) for x in model_datas]
 
         # 1. Create variables
         p = LpVariable.matrix("p", (range(N), range(M)), cat="Binary")
@@ -77,7 +98,7 @@ class SelectiveReplication(PlacementPolicy):
         # 3. Constraints
         # (a). memory budget on each GPU
         for j in range(M):
-            prob += lpSum(p[i][j] * c[i] for i in range(N)) <= C
+            prob += lpSum(p[i][j] * (c[i] / C) for i in range(N)) <= 1
 
         # (b). number of replicas
         for i in range(N):
@@ -86,7 +107,7 @@ class SelectiveReplication(PlacementPolicy):
         # (c). min tolerance and sum tolerance
         for i in range(N):
             prob += min_tolerance <= rep[i] * (t[i] / a[i])
-        prob += sum_tolerance == lpSum(rep[i] for i in range(N))
+        prob += sum_tolerance == lpSum(rep[i] * (t[i] / a[i]) for i in range(N))
 
         msg = self.verbose
         assert "PULP_CBC_CMD" in pulp.listSolvers(onlyAvailable=True), (
@@ -124,65 +145,75 @@ class SelectiveReplicationWithPipeline(PlacementPolicy):
         self.verbose = verbose
         self.time_limit = 20
         self.sum_k = 1e-4
+        self.max_bs = 1
 
-    def place_models(self,
-                     controller,
-                     model_datas: List[ModelData],
-                     num_gpus: int,
-                     mem_budget: float,
-                     group_sizes: List[int]):
-        obj, (group_sizes, group_models) = self.solve(
-            model_datas, num_gpus, mem_budget, group_sizes)
-        num_groups = len(group_sizes)
+        self.group_configs = [
+            ParallelConfig(1, 1, 1),
+            ParallelConfig(1, 1, 2),
+            ParallelConfig(1, 1, 4),
+            ParallelConfig(1, 1, 8),
+        ]
+        self.group_sizes = [
+            np.prod(*x) for x in self.group_configs
+        ]
 
-        # Launch mesh groups
-        for g_id in range(num_groups):
-            pp_size = group_sizes[g_id]
-            controller.launch_mesh_group_manager.remote(g_id, [1, pp_size])
+    def compute_capability(self, model_data, parallel_config):
+        slo = model.slo
+        latency_mem = model.profiling_result.para_dict.get(parallel_config, None)
 
-        # Launch model replicas
-        tasks = []
-        for g_id in range(num_groups):
-            pp_size = group_sizes[g_id]
-            for m_id in group_models[g_id]:
-                name = model_datas[m_id].name
-                tasks.append(controller.create_replica.remote(
-                    name, g_id, [ParallelConfig(1, 1, pp_size)]))
+        if latency_mem is None:
+            return 0
 
-        return tasks
+        max_cap = 0
+        for b, ls in latency_mem.latency.items():
+            if b > self.max_bs:
+                continue
+
+            # slo = sum(ls) + (n-1) * max(ls)
+            # so, n = ceil((slo - sum(ls)) / max(ls)) + 1
+            max_cap = max(max_cap, (slo - sum(ls)) // max(ls) + 1)
+
+        return max_cap
+
+    def compute_max_stage_mem(self, model_data, parallel_config, mem_budget):
+        slo = model.slo
+        latency_mem = model.profiling_result.para_dict.get(parallel_config, None)
+
+        if latency_mem is None:
+            return mem_budget * 2
+
+        return max(latency_mem.weight_mem)
 
     def solve(self,
               model_datas: List[ModelData],
-              num_gpus: int,
-              mem_budget: float,
-              group_sizes: List[int]):
+              cluster_env: ClusterEnv):
         tic = time.time()
 
         # Load constants
         N = len(model_datas)
-        M = num_gpus
+        M = cluster_env.num_gpus
         C = 1
         a = [x.average_load for x in model_datas]
-        c = [x.model_mem / mem_budget for x in model_datas]
-        t = [x.single_throughput for x in model_datas]
+        c = [x.profiling_result.para_dict[ParallelConfig(1,1,1)].weight_mem[0]
+             for x in model_datas]
 
         G = num_gpus
-        K = len(group_sizes)
-        g = group_sizes
+        K = len(self.group_configs)
+        g = self.group_sizes
+        f = np.zeros((N, K))
         d = np.zeros((N, K))
         for i in range(N):
-            for group_size, decay in model_datas[i].pipeline_decay:
-                try:
-                    k = g.index(group_size)
-                except ValueError:
-                    k = -1
-
-                if k >= 0:
-                    d[i][k] = decay
+            model_data = model_datas[i]
+            for k in range(K):
+                parallel_config = self.group_configs[k]
+                f[i][k] = self.compute_capability_mem(
+                    model_data, parallel_config)
+                d[i][k] = self.compute_max_stage_mem(
+                    model_data, parallel_config)
 
         # 1. Create variables
         p = LpVariable.matrix("p", (range(N), range(G)), cat="Binary")
-        rep = [None] * N
+        cap = [None] * N
         min_tolerance = LpVariable("min_tolerance", lowBound=0)
         sum_tolerance = LpVariable("sum_tolerance", lowBound=0)
         s = LpVariable.matrix("s", (range(G), range(K)), cat="Binary")
@@ -196,19 +227,24 @@ class SelectiveReplicationWithPipeline(PlacementPolicy):
         # 3. Constraints
         # (a). memory budget on each GPU
         for j in range(G):
-            prob += (lpSum(p[i][j] * c[i] for i in range(N)) <=
-                     C * lpSum(s[j][k] * g[k] for k in range(K)))
+            prob += (lpSum(p[i][j] * (c[i] / C) for i in range(N)) <=
+                     lpSum(s[j][k] * g[k] for k in range(K)))
 
-        # (b). number of replicas
+        ## A more precise version, not used right now
+        #for j in range(G):
+        #    prob += (lpSum(pxs[i][j][k] * (d[i][k] / C)
+        #                   for i in range(N) for k in range(K)) <= 1)
+
+        # (b). capability
         for i in range(N):
-            rep[i] = lpSum(pxs[i][j][k] * g[k] * d[i][k]
+            cap[i] = lpSum(pxs[i][j][k] * f[i][k]
                            for j in range(G) for k in range(K))
 
         # (c). min tolerance and sum tolerance
         for i in range(N):
-            prob += min_tolerance <= rep[i] * t[i] / a[i]
+            prob += min_tolerance <= cap[i] / a[i]
 
-        prob += sum_tolerance == lpSum(rep[i] for i in range(N))
+        prob += sum_tolerance == lpSum(cap[i] / a[i] for i in range(N))
 
         # (d). group size
         prob += lpSum(s[j][k] * g[k] for j in range(G) for k in range(K)) == M
@@ -248,6 +284,7 @@ class SelectiveReplicationWithPipeline(PlacementPolicy):
                 "Cannot run the function under the given memory budget. "
                 "Please increase the memory budget.")
 
+        # Group configuration selection
         s_res = []
         for j in range(G):
             assert sum(pulp.value(s[j][k]) for k in range(K)) == 1
@@ -255,22 +292,23 @@ class SelectiveReplicationWithPipeline(PlacementPolicy):
                 if pulp.value(s[j][k]):
                     s_res.append(k)
 
+        # Placement
         p_res = np.zeros((N, G), dtype=np.int8)
         for i in range(N):
             for j in range(G):
                 if pulp.value(p[i][j]):
                     p_res[i][j] = 1
 
-        group_sizes = []
+        group_configs = []
         group_models = []
         for j in range(G):
-            group_size = g[s_res[j]]
-            if group_size:
+            config_id = s_res[j]
+            if self.group_size[config_id]:
                 tmp = []
                 for i in range(N):
                     if p_res[i][j]:
                         tmp.append(i)
-                group_sizes.append(group_size)
+                group_configs.append(self.group_configs[config_id])
                 group_models.append(tmp)
 
-        return objective, (group_sizes, group_models)
+        return objective, (group_configs, group_models)
--- tests/serve/test_placement_policy.py
+++ tests/serve/test_placement_policy.py
@@ -3,8 +3,9 @@ import unittest
 
 import numpy as np
 
-from alpa_serve.placement_policy import (SelectiveReplication, ModelData,
-    SelectiveReplicationWithPipeline)
+from alpa_serve.placement_policy import (ModelData, ClusterEnv,
+    SelectiveReplication, SelectiveReplicationWithPipeline)
+from alpa_serve.profiling import load_test_prof_result
 from alpa.util import GB
 
 
@@ -13,16 +14,15 @@ class PlacementPolicyTest(unittest.TestCase):
     def test_selective_replication(self):
         policy = SelectiveReplication()
 
-        mem_budget = 2 * GB
-        num_gpus = 4
+        cluster_env = ClusterEnv(num_gpus=4, mem_budget=4.5*GB)
         model_datas = [
-            ModelData("m1", 1.0 * GB, 1.0, 1.0),
-            ModelData("m2", 1.0 * GB, 1.0, 1.0),
-            ModelData("m3", 1.0 * GB, 1.0, 1.0),
-            ModelData("m4", 1.0 * GB, 1.0, 1.0),
+            ModelData("m1", 2, 5, load_test_prof_result("test-2GB-100ms")),
+            ModelData("m2", 2, 5, load_test_prof_result("test-2GB-100ms")),
+            ModelData("m3", 2, 5, load_test_prof_result("test-2GB-100ms")),
+            ModelData("m4", 2, 5, load_test_prof_result("test-2GB-100ms")),
         ]
 
-        obj, placement = policy.solve(model_datas, num_gpus, mem_budget)
+        obj, placement = policy.solve(model_datas, cluster_env)
 
         row_sum = np.sum(placement, axis=1)
         col_sum = np.sum(placement, axis=0)
@@ -30,26 +30,24 @@ class PlacementPolicyTest(unittest.TestCase):
         assert col_sum.tolist() == [2, 2, 2, 2]
 
     def test_selective_replication_with_pipeline(self):
-        mem_budget = 2 * GB
-        num_gpus = 4
+        cluster_env = ClusterEnv(num_gpus=4, mem_budget=4*GB)
         model_datas = [
-            ModelData("m1", 1.0 * GB, 1.0, 1.0, [(1, 1), (2, 0.95), (4, 0.9)]),
-            ModelData("m2", 1.0 * GB, 1.0, 1.0, [(1, 1), (2, 0.95), (4, 0.9)]),
-            ModelData("m3", 1.0 * GB, 1.0, 1.0, [(1, 1), (2, 0.95), (4, 0.9)]),
-            ModelData("m4", 1.0 * GB, 1.0, 1.0, [(1, 1), (2, 0.95), (4, 0.9)]),
+            ModelData("m1", 2, 5, load_test_prof_result("test-2GB-100ms")),
+            ModelData("m2", 2, 5, load_test_prof_result("test-2GB-100ms")),
+            ModelData("m3", 2, 5, load_test_prof_result("test-2GB-100ms")),
+            ModelData("m4", 2, 5, load_test_prof_result("test-2GB-100ms")),
         ]
 
         policy = SelectiveReplicationWithPipeline()
-        obj, (group_sizes, group_models) = policy.solve(
-            model_datas, num_gpus, mem_budget, [0, 1, 2, 4])
+        obj, (group_configs, group_models) = policy.solve(model_datas, cluster_env)
 
-        assert group_sizes == [2, 2]
-        assert group_models == [[0, 1, 2, 3], [0, 1, 2, 3]]
+        print(group_configs)
+        print(group_models)
 
 
 def suite():
     suite = unittest.TestSuite()
-    suite.addTest(PlacementPolicyTest("test_selective_replication"))
+    #suite.addTest(PlacementPolicyTest("test_selective_replication"))
     suite.addTest(PlacementPolicyTest("test_selective_replication_with_pipeline"))
     return suite
 
@@ -57,4 +55,3 @@ def suite():
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
     runner.run(suite())
-
