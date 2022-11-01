a6aab0ebe203651cd1cdff40367dbda9b3f43825
Yinmin Zhong <zhongyinmin@pku.edu.cn>
2022-11-01 20:10:27 +0800

    profile per-stage weight/activation memory

--- alpa_serve/profiling.py
+++ alpa_serve/profiling.py
@@ -23,7 +23,11 @@ class LatencyMemData:
     act_mem: Dict
     # The weight memory of each stage
     # Type: List[stage_weight_mem]
-    weight_mem: Dict
+    weight_mem: List
+
+    def add_result(self, batch_size: int, latency: List[float], act_mem: List[float]):
+        self.latency[batch_size] = latency
+        self.act_mem[batch_size] = act_mem
 
 
 @dataclasses.dataclass
@@ -38,9 +42,16 @@ class ProfilingResult:
     # The latency of postprocess on CPU.
     postprocess_cpu: float
 
-    def add_result(self, parallel_config: ParallelConfig, batch_size: int, stage_latency: List[float], act_mem: float):
+    def add_result(self, parallel_config: ParallelConfig, batch_size: int,
+                   stage_latency: List[float], act_mem: List[float], weight_mem: List[float]):
         """Add or overwrite the profiling results of a model."""
-        raise NotImplementedError
+        if parallel_config not in self.para_dict:
+            self.para_dict[parallel_config] = LatencyMemData(
+                latency={batch_size: stage_latency},
+                act_mem={batch_size: act_mem},
+                weight_mem=weight_mem)
+        else:
+            self.para_dict[parallel_config].add_result(batch_size, stage_latency, act_mem)
 
 
 class ProfilingDatabase:
@@ -63,24 +74,39 @@ class ProfilingDatabase:
     def _extract_data(self, row):
         """Extract the profiling results from a row of the profiling CSV file."""
         stage_latencies = list(map(float, row["StageLatencies(s)"].strip("[]").split()))
+        weight_mem = list(map(float, row["StageWeights(GB)"].strip("[]").split()))
+        peak_mem = list(map(float, row["StagePeakMem(GB)"].strip("[]").split()))
         # TODO: fix the activation memory
-        act_mem = {}
+        act_mem = [peak_mem - weight_mem for peak_mem, weight_mem in zip(peak_mem, weight_mem)]
         parallel_config = ParallelConfig(int(row["DP"]), int(row["OP"]), int(row["PP"]))
-        return row["ModelName"], parallel_config, int(row["BS"]), stage_latencies, act_mem, float(row["Weights(GB)"])
+        return row["ModelName"], parallel_config, int(row["BS"]), stage_latencies, weight_mem, act_mem
 
     def update_from_csv(self, file_name: str):
         results = {}
         with open(file_name, "r") as f:
-            reader = csv.DictReader(f, delimiter=",")
+            reader = csv.DictReader(f, delimiter="\t")
             for row in reader:
-                model_name, parallel_config, batch_size, stage_latencies, act_mem, weight_mem = self._extract_data(row)
+                model_name, parallel_config, batch_size, stage_latencies, weight_mem, act_mem = self._extract_data(row)
                 if model_name not in results:
-                    results[model_name] = ProfilingResult(model_name,
-                                                          {parallel_config: {batch_size: stage_latencies}},
-                                                          0, 0, act_mem, weight_mem)
+                    results[model_name] = ProfilingResult(
+                                                            model_name,
+                                                            {
+                                                                parallel_config: LatencyMemData(
+                                                                    latency={   # Dict[batch_size -> List[stage_latency]]
+                                                                        batch_size: stage_latencies,
+                                                                    },
+                                                                    act_mem={   # Dict[batch_size -> List[stage_act_mem]]
+                                                                        batch_size: act_mem,
+                                                                    },
+                                                                    weight_mem=weight_mem # List[stage_weight_mem]
+                                                                )
+                                                            },
+                                                            preprocess_cpu=0.0,
+                                                            postprocess_cpu=0.0
+                                                        )
                 else:
-                    results[model_name].add_result(parallel_config, batch_size, stage_latencies, act_mem)
-        self.results = results
+                    results[model_name].add_result(parallel_config, batch_size, stage_latencies, act_mem, weight_mem)
+        self.results.update(results)
 
     def materialize(self):
         """Write the profiling results to the database file."""
@@ -230,3 +256,7 @@ def load_test_prof_result(name: str):
         )
     else:
         raise ValueError("Unsupported model: {name}")
+
+# database = ProfilingDatabase("profiling_result.pkl", False)
+# bert_1_3b = database.get("bert-1.3b")
+# print(bert_1_3b.para_dict[ParallelConfig(1, 4, 4)].latency[8])
