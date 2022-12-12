3c4c3ce0748b6bccc05e82f36bb08bb5abb07bda
Ying Sheng <sqy1415@gmail.com>
2022-12-12 00:40:46 +0000

    update

---
+++ experiments/e2e_goodput/plot_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python3 plot_various_metrics.py --input all_transformers_all/res_general_vs_all.tsv --general
+
+python3 plot_various_metrics.py --input mixed_all/res_general_vs_all.tsv --general
+
