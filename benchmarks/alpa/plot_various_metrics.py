78464dd6b753ac445c42415516be7fd0acbc5404
flyingpig <60659728+PKUFlyingPig@users.noreply.github.com>
2022-12-13 02:10:59 +0800

    Plot scripts for section 6.2, 6.3, 6.4 (#85)
    * sec6.2, 6.3
    
    * change policy name
    
    * sec6.4 plot

--- benchmarks/alpa/plot_various_metrics.py
+++ benchmarks/alpa/plot_various_metrics.py
@@ -8,28 +8,29 @@ import matplotlib.pyplot as plt
 from benchmarks.alpa.equal_model_case import read_equal_model_case_tsv
 
 show_name_dict = {
-    "sr-greedy":   "Selective Replication (greedy)",
+    "mp-ilp":         "Model Parallelism (ilp)",
+    "mp-search":      "Beta",
+    "mp-search-sep":  "Model Parallelism (search model separation)",
+
+    "sr-replace-30": "Clockwork++",
+    "sr-replace-60": "Clockwork++",
+    "sr-replace-3600": "Clockwork++",
+    "sr-replace-5400": "Clockwork++",
+    "sr-replace-10800": "Clockwork++",
+    "sr-replace-21600": "Clockwork++",
+
+    "sr-greedy":   "Selective Replication",
     "sr-search":   "Selective Replication (search)",
     "sr-ilp":      "Selective Replication (ilp)",
-    "sr-replace-30": "Clockwork++ (30)",
-    "sr-replace-60": "Clockwork++ (60)",
-    "sr-replace-3600": "Clockwork++ (3600)",
-    "sr-replace-5400": "Clockwork++ (5400)",
-    "sr-replace-10800": "Clockwork++ (10800)",
-    "sr-replace-21600": "Clockwork++ (21600)",
-
-    "mp-ilp":         "Model Parallelism (ilp)",
-    "mp-search":      "Model Parallelism (search)",
-    "mp-search-sep":  "Model Parallelism (search model separation)",
 
     "mp-greedy-2":    "Pipeline Parallelism (#stage=2)",
     "mp-greedy-4":    "Pipeline Parallelism (#stage=4)",
     "mp-greedy-8":    "Pipeline Parallelism (#stage=8)",
     "mp-greedy-16":   "Pipeline Parallelism (#stage=16)",
-    "mp-equal-16-1":  "Model Parallelism (pp=16, op=1)", 
-    "mp-equal-8-2":   "Model Parallelism (pp=8, op=2)",
-    "mp-equal-4-4":   "Model Parallelism (pp=4, op=4)",
-    "mp-equal-2-8":   "Model Parallelism (pp=2, op=8)",
+    "mp-equal-16-1":  "(16,1)", 
+    "mp-equal-8-2":   "(8,2)",
+    "mp-equal-4-4":   "(4,4)",
+    "mp-equal-2-8":   "(2,8)",
 }
 
 def show_name(name):
@@ -54,10 +55,10 @@ def method2color(name):
 
 
 method_order_list = [
-    "sr-greedy", "sr-search", "sr-ilp",
-    "sr-replace-30", "sr-replace-60", "sr-replace-3600", "sr-replace-5400", "sr-replace-10800", "sr-replace-21600",
-
     "mp-ilp", "mp-search", "mp-search-sep",
+    "sr-replace-30", "sr-replace-60", "sr-replace-3600", "sr-replace-5400", "sr-replace-10800", "sr-replace-21600",
+    "sr-greedy", "sr-search", "sr-ilp",
+
     "mp-search-100", "mp-search-1000",
     "mp-greedy-2", "mp-greedy-4", "mp-greedy-8", "mp-greedy-16",
     "mp-equal-16-1", "mp-equal-8-2", "mp-equal-4-4", "mp-equal-2-8",
