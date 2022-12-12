2ab0b0815c07591b3e132a66d3b6b3f1929aa4f8
Ying Sheng <sqy1415@gmail.com>
2022-12-12 00:17:20 +0000

    update

--- experiments/e2e_goodput/run_all.sh
+++ experiments/e2e_goodput/run_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 general_model_exp.py --exp-ids=all --parallel --exp-name=all_transformers_all output=res_general_vs_all.tsv --trace-dir=/home/ubuntu/azure_v2.pkl --workload=azure_v2 --model-type=all_transformers
+python3 general_model_exp.py --exp-ids=all --parallel --exp-name=all_transformers_all --output=res_general_vs_all.tsv --trace-dir=/home/ubuntu/azure_v2.pkl --workload=azure_v2 --model-type=all_transformers
 
 
 python3 general_model_exp.py --exp-ids=all --parallel --exp-name=mixed_all --output=res_general_vs_all.tsv --trace-dir=/home/ubuntu/azure_v2.pkl --workload=azure_v2 --model-type=mixed
